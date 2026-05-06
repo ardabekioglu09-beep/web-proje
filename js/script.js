80dacf9457a80437ed26b122600996507e5d0184
Computer.eng.ArdaBekiroglu <ardabekioglu09@gmail.com>
2026-05-07 00:26:36 +0300

    Gün 2: ilgi alanlari sayfasi gelistirildi ve API entegrasyonu eklendi

--- js/script.js
+++ js/script.js
@@ -28,3 +28,102 @@ function prevSlide() {
     index = (index - 1 + images.length) % images.length;
     showSlide(index);
 }
+const games = [
+    {
+        name: "Cyberpunk 2077",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg",
+        discount: 60,
+        oldPrice: "999 TL",
+        newPrice: "399 TL"
+    },
+
+    {
+        name: "Resident Evil 4",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/2050650/header.jpg",
+        discount: 0,
+        oldPrice: "",
+        newPrice: ""
+    },
+    {
+        name: "Amnesia: The Bunker",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1944430/header.jpg",
+        discount: 40,
+        oldPrice: "299 TL",
+        newPrice: "179 TL"
+    },
+    {
+        name: "Call of Duty 4: Modern Warfare",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/7940/header.jpg",
+        discount: 75,
+        oldPrice: "199 TL",
+        newPrice: "49 TL"
+    }
+];
+
+const gameContainer = document.getElementById("game-data");
+
+if (gameContainer) {
+    games.forEach(game => {
+
+        let discountText = "";
+
+        if (game.discount > 0) {
+
+            discountText = `
+            <p class="discount">%${game.discount} İndirim</p>
+            <p><del>${game.oldPrice}</del></p>
+            <p>${game.newPrice}</p>
+        `;
+
+        } else {
+
+            discountText = `
+            <p class="no-discount">Şu anda indirim yok</p>
+        `;
+        }
+
+        gameContainer.innerHTML += `
+        <div class="game-card">
+
+            <img src="${game.image}">
+
+            <h2>${game.name}</h2>
+
+            ${discountText}
+
+        </div>
+    `;
+    });
+}
+const apiKey = "24f045ee0ae8453dbb8d60e188f2b47f";
+const newsContainer = document.getElementById("news-container");
+
+if (newsContainer) {
+    fetch(`https://newsapi.org/v2/everything?q=technology OR gaming OR NVIDIA OR Tesla&language=tr&sortBy=publishedAt&pageSize=5&apiKey=${apiKey}`)
+
+    .then(response => response.json())
+
+    .then(data => {
+
+        if (!data.articles) {
+            return;
+        }
+
+        data.articles.forEach(article => {
+
+            newsContainer.innerHTML += `
+            <div class="news-card">
+                <h2>${article.title}</h2>
+                <p>${article.description || "Açıklama bulunamadı."}</p>
+                <a href="${article.url}" target="_blank">
+                    Haberi Oku
+                </a>
+            </div>
+            `;
+        });
+
+    })
+    .catch(error => {
+        console.error("News API hatası:", error);
+    });
+}
